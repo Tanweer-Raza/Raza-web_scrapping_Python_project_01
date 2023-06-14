7b3acc8f5b4f14eb1284d9e4e0fa4d9b34b21c61
Tanweer Raza <tanatif@gmail.com>
2023-06-14 15:41:36 +0530

    updated for deployment

--- application.py
+++ application.py
@@ -94,5 +94,6 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    # app.run(host='127.0.0.1', port=8001, debug=True)    For local system
+    app.run(host='0.0.0.0', port=8001, debug=True)
 	#app.run(debug=True)
